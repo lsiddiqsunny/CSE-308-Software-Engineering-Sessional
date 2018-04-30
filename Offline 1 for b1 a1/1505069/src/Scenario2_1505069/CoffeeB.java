7b2730a847cd28d388b9f617b1433e959387d857
lsiddiqsunny <lsiddiqsunny@gmail.com>
2018-04-30 22:18:16 +0600

    Update class diagram and solution for offline 2/problem 1

--- Offline 1 for b1 a1/1505069/src/Scenario2_1505069/CoffeeB.java
+++ Offline 1 for b1 a1/1505069/src/Scenario2_1505069/CoffeeB.java
@@ -3,6 +3,6 @@ package Scenario2_1505069;
 public class CoffeeB implements Coffee {
     @Override
     public String haveSugar() {
-        return "Coffee B with sugar";
+        return "Coffee B with no sugar";
     }
 }
--- Offline 1 for b1 a1/1505069/src/Scenario2_1505069/CoffeeC.java
+++ Offline 1 for b1 a1/1505069/src/Scenario2_1505069/CoffeeC.java
@@ -3,6 +3,6 @@ package Scenario2_1505069;
 public class CoffeeC implements Coffee {
     @Override
     public String haveSugar() {
-        return "Coffee C with no sugar";
+        return "Coffee C with  sugar";
     }
 }
